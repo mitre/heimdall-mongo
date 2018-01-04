acde05ecada7f72462b0b390fc4c1594d809d24c
Matthew Dromazos <dromazmj@dukes.jmu.edu>
2018-01-04 18:11:02 -0500

    * Adds functionality for viewing all mappings, a single mapping * adds functionality to add a new mapping from an excel * adds functionality to edit an entry in a mapping
    Signed-off-by: Matthew Dromazos <dromazmj@dukes.jmu.edu>

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
+require 'csv'
+require 'iconv'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,14 @@ Rails.application.routes.draw do
     resources :profile_attributes
     resources :groups
   end
+  resources :mappings
   resources :evaluations do
     resources :results
   end
 
   match 'profile_upload' => 'profiles#upload', :as => :upload_profile, :via => :post
   match 'evaluation_upload' => 'evaluations#upload', :as => :upload_evaluation, :via => :post
+  match 'mapping_upload' => 'mappings#upload', :as => :upload_mapping, :via => :post
   # Root path (/)
   root :to => 'dashboard#index'
 end
